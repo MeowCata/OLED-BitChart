6e0964b65162602d58a9ccd28bb72009e1bfbb9a
DingDang <97514372+MeowCata@users.noreply.github.com>
2025-06-27 20:34:44 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,11 @@
 // tests go here; this will not be compiled when this package is used as an extension.
+YFOLED.init(128, 64)
+basic.forever(function () {
+    input.onButtonPressed(Button.A, function () {
+        while (!input.buttonIsPressed(Button.B)) {
+            BitChart.drawCurve(Math.round(input.lightLevel())*100)/100, 5, 10)
+        }
+        YFOLED.clear()
+        BitChart.resetGraph()
+    })
+})
